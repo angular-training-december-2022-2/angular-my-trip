4f7208b5fd6c057cf8ecae1e934c72740b2b8fa2
Poonga <bpoongs1982@gmail.com>
2023-01-27 22:24:34 -0500

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,9 +15,11 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
+    //const app1 = new AppComponent();
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeTruthy(); // here we are testing that throught the TestBed are we able to crete an object/instance of AppComponent
+                              // this assertion will pass if the object/instance was created successfully  
   });
 
   it(`should have as title 'angular-dec-17-my-trip'`, () => {
@@ -28,8 +30,8 @@ describe('AppComponent', () => {
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges(); // this will run the change detection cycle
+    const compiled = fixture.nativeElement as HTMLElement; // this will give a reference to the template
     expect(compiled.querySelector('.content span')?.textContent).toContain('angular-dec-17-my-trip app is running!');
   });
 });
